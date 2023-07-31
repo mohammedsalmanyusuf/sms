f9198d5fb2302097e8d00dd71f959efce7c3673c
mohammedsalmanyusuf <141034658+mohammedsalmanyusuf@users.noreply.github.com>
2023-07-31 18:04:15 +0530

    Add files via upload

---
+++ color.py
@@ -0,0 +1,7 @@
+color_1 = "yellow"
+color_2 = "skyblue"
+color_3 = "black"
+color_4 = "white"
+
+font_1 = "times new roman"
+font_2 = "helvetica"
---
+++ database.py
@@ -0,0 +1,5 @@
+# User database
+host = 'localhost'
+user = 'root'
+password = 'w@2915djkq#'
+database = 'student_management'
